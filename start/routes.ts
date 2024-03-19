af533e9f81a4825f7838f7fd12f20032715c4752
arthur <arthur.broudoux@gmail.com>
2024-03-19 09:46:49 +0100

    create table and make mgration

--- start/routes.ts
+++ start/routes.ts
@@ -8,5 +8,7 @@
 */
 
 import router from '@adonisjs/core/services/router'
+const TodosController = () => import('#controllers/todos_controller')
 
 router.on('/').render('pages/home')
+router.get('/todos', [TodosController, 'index'])
