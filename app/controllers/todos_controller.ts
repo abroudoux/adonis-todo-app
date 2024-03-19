087decf036017730b04aa7d4416821dd0df3cd45
arthur <arthur.broudoux@gmail.com>
2024-03-19 14:32:16 +0100

    commit

--- app/controllers/todos_controller.ts
+++ app/controllers/todos_controller.ts
@@ -6,4 +6,42 @@ export default class TodosController {
     const todos = await Todo.all()
     return view.render('todos/index', { todos })
   }
+
+  async show({ params, view }: HttpContext) {
+    const todo = await Todo.find(params.id)
+    return view.render('todos/show', { todo })
+  }
+
+  async edit({ params, view }: HttpContext) {
+    const todo = await Todo.find(params.id)
+    return view.render('todos/edit', { todo })
+  }
+
+  async update({ params, request, response }: HttpContext) {
+    const todo = await Todo.find(params.id)
+    if (todo) {
+      todo.title = request.input('title')
+      todo.description = request.input('description')
+      await todo.save()
+    }
+    return response.redirect().toRoute('todos.index')
+  }
+
+  async create({ view }: HttpContext) {
+    return view.render('todos/create')
+  }
+
+  async store({ request, response }: HttpContext) {
+    const todo = new Todo()
+    todo.title = request.input('title')
+    todo.description = request.input('description')
+    await todo.save()
+    return response.redirect().toRoute('todos.index')
+  }
+
+  async destroy({ params, response }: HttpContext) {
+    const todo = await Todo.find(params.id)
+    await todo?.delete()
+    return response.redirect().toRoute('todos.index')
+  }
 }
--- start/routes.ts
+++ start/routes.ts
@@ -11,4 +11,13 @@ import router from '@adonisjs/core/services/router'
 const TodosController = () => import('#controllers/todos_controller')
 
 router.on('/').render('pages/home')
+
 router.get('/todos', [TodosController, 'index'])
+router.get('/todos/create', [TodosController, 'create'])
+router.get('/todos/:id', [TodosController, 'show'])
+router.get('/todos/:id/edit', [TodosController, 'edit'])
+router.put('/todos/:id/update', [TodosController, 'update'])
+
+router.post('/todos', [TodosController, 'store'])
+
+router.delete('/todos/:id', [TodosController, 'destroy'])
